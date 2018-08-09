de30308bd8ddc29f73b33fe182df32d65331da4d
Tareq3 <mti.tareq3@gmail.com>
2018-08-09 20:57:27 +0600

    Menu Added

--- app/src/main/java/com/mti/customisedstatusbar/MainActivity.java
+++ app/src/main/java/com/mti/customisedstatusbar/MainActivity.java
@@ -8,12 +8,30 @@ package com.mti.customisedstatusbar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
+
+   //Member Variable
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_with_appbar);
+
+        toolbar  =  findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitle("Home");
+        toolbar.setLogo(R.drawable.ic_home_white_24dp);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+
     }
 }
