95c429cb376bd24d0ae7997223a8fd2d36d5b7bb
Tareq3 <mti.tareq3@gmail.com>
2018-08-09 20:18:22 +0600

    Initial commit

---
+++ app/src/main/java/com/mti/customisedstatusbar/MainActivity.java
@@ -0,0 +1,19 @@
+/*
+ * Created by Tareq Islam on 8/9/18 4:31 PM
+ *
+ *  Last modified 8/9/18 4:17 PM
+ */
+
+package com.mti.customisedstatusbar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_with_appbar);
+    }
+}
