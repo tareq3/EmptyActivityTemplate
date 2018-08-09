0ccff7adb0f552c953662f25b609b76fa4567a19
tareq3 <mti.tareq3@gmail.com>
2018-08-09 21:16:27 +0600

    Update MainActivity.java

--- app/src/main/java/com/mti/customisedstatusbar/MainActivity.java
+++ app/src/main/java/com/mti/customisedstatusbar/MainActivity.java
@@ -9,11 +9,17 @@ package com.mti.customisedstatusbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.support.v7.widget.Toolbar;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_with_appbar);
+        
+        Toolbar toolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitle("Home");
     }
 }
